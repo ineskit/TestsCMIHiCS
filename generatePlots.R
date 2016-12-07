2978becb17b2d9142ca93b8e8874bb9698310471
Ines <uceop@student.kit.edu>
2016-12-07 15:37:51 +0100

    separate file (generatePlots.R) with code chunks
    separate file (generatePlots.R) with code chunks

--- generatePlots.R
+++ generatePlots.R
@@ -4,127 +4,200 @@ library(stringr)
 library(data.table)
 library(tikzDevice)
 
+## ---- loadData
+colSSVec <- c(1,2,36:55) # top 20 subspaces as vectors
+colTop5SS <- c(1,2,16:20)   # top 5 subspaces as characters
 
-## ---- plotTest
+load("results/Param_100_050_020_SSVec/experimentResult_ann_161129.RData")
+annSS <-combinedResult[,colSSVec, with=FALSE]
+annSSU <-annSS[!duplicated(annSS$algorithm)]
+ann5SS <-combinedResult[!duplicated(combinedResult[,colTop5SS, with=FALSE]),colTop5SS, with=FALSE]
+tmp <- combinedResult 
 
- load("results/Param_100_050_020/experimentResult_glass_161102_100_50_20.RData") # glass
- tmp <- combinedResult 
- load("results/Param_100_050_020/experimentResult_ann_diabet_ion_161102_100_50_20.RData") # ann, diabetes, ion
- tmp <- rbind(tmp, combinedResult)
- load("results/Param_100_050_020/experimentResult_pen_lymph_segm_161102_100_50_20.RData") # pen, lymph, segment
- tmp <- rbind(tmp, combinedResult)
- combinedResult <- tmp
- 
- 
- plotThemeReal <- theme_bw() +
-   theme(plot.title = element_text(size=40, vjust = 2.5, face = "bold"),
-         axis.title.x = element_text(margin = margin(15,0,0,0)),
-         axis.title.y = element_text(margin = margin(0,15,0,0)),
-         axis.text = element_text(margin = margin(0,0,0,0)),
-         axis.ticks.y = element_blank(),
-         axis.ticks.x = element_blank(),
-         plot.margin = unit(c(0, 0, 0.3, 0), "lines"),
-         panel.border = element_blank(),
-         panel.grid.major.x = element_blank(),
-         panel.grid.major.y = element_line( size=.3, color="black"),
-         legend.key = element_rect(size=1, color="white"),
-         legend.key.size = unit(1, "lines"),
-         legend.key.height=unit(1,"lines"),
-         legend.title = element_blank(),
-         legend.position = "right",
-         strip.background = element_blank(),
-         strip.text = element_text(size = 12),
-         panel.margin.x=unit(1.5, "lines"))
- 
- ggplot(combinedResult, aes(x=dataset , y=AUC_sum, fill=algorithm)) +
-   geom_boxplot() +
-   facet_wrap(~dataset) + 
-   xlab(label = "Datasets") +
-   ylab(label = "AUC") +
-   plotThemeReal  +
-   scale_y_continuous(limits = c(0.5,1)) # +
-   # scale_fill_grey(start = 0.1, end = 0.7) 
- 
- 
+# load("results/Param_100_050_020_SSVec/experimentResult_ann_161129.RData")
+# breastSS <-combinedResult[,colSSVec, with=FALSE]
+# breastU <-breastSS[!duplicated(breastSS$algorithm)]
+# breast5SS <-combinedResult[!duplicated(combinedResult[,colTop5SS, with=FALSE]),colTop5SS, with=FALSE]
+# tmp <- rbind(tmp, combinedResult)
+
+load("results/Param_100_050_020_SSVec/experimentResult_diabetes_161206.RData")
+diabSS <-combinedResult[,colSSVec, with=FALSE]
+diabSSU <-diabSS[!duplicated(diabSS$algorithm)]
+diab5SS <-combinedResult[!duplicated(combinedResult[,colTop5SS, with=FALSE]),colTop5SS, with=FALSE]
+tmp <- rbind(tmp, combinedResult)
+
+load("results/Param_100_050_020_SSVec/experimentResult_glass_161129.RData")
+glassSS <-combinedResult[,colSSVec, with=FALSE]
+glassSSU <-glassSS[!duplicated(glassSS$algorithm)]
+glass5SS <-combinedResult[!duplicated(combinedResult[,colTop5SS, with=FALSE]),colTop5SS, with=FALSE]
+tmp <- rbind(tmp, combinedResult)
+
+load("results/Param_100_050_020_SSVec/experimentResult_ion_161129.RData")
+ionSS <-combinedResult[,colSSVec, with=FALSE]
+ionSSU <-ionSS[!duplicated(ionSS$algorithm)]
+ion5SS <-combinedResult[!duplicated(combinedResult[,colTop5SS, with=FALSE]),colTop5SS, with=FALSE]
+tmp <- rbind(tmp, combinedResult)
+
+load("results/Param_100_050_020_SSVec/experimentResult_lymph_161206.RData")
+lympSS <-combinedResult[,colSSVec, with=FALSE]
+lympSSU <-lympSS[!duplicated(lympSS$algorithm)]
+lymp5SS <-combinedResult[!duplicated(combinedResult[,colTop5SS, with=FALSE]),colTop5SS, with=FALSE]
+tmp <- rbind(tmp, combinedResult)
+
+load("results/Param_100_050_020_SSVec/experimentResult_pen_161206.RData")
+penSS <-combinedResult[,colSSVec, with=FALSE]
+penSSU <-penSS[!duplicated(penSS$algorithm)]
+pen5SS <-combinedResult[!duplicated(combinedResult[,colTop5SS, with=FALSE]),colTop5SS, with=FALSE]
+tmp <- rbind(tmp, combinedResult)
+
+load("results/Param_100_050_020_SSVec/experimentResult_segmentn_161206.RData")
+segmSS <-combinedResult[,colSSVec, with=FALSE]
+segmSSU <-segmSS[!duplicated(segmSS$algorithm)]
+segm5SS <-combinedResult[!duplicated(combinedResult[,colTop5SS, with=FALSE]),colTop5SS, with=FALSE]
+combinedResult <- rbind(tmp, combinedResult)
+
 ## ---- end
 
-## ---- plotRealWorldData
-# 
-# load("results/Param_100_050_020/experimentResult_glass_161102_100_50_20.RData") # glass
-# tmp <- combinedResult 
-# load("results/Param_100_050_020/experimentResult_ann_diabet_ion_161102_100_50_20.RData") # ann, diabetes, ion
-# tmp <- rbind(tmp, combinedResult)
-# load("results/Param_100_050_020/experimentResult_pen_lymph_segm_161102_100_50_20.RData") # pen, lymph, segment
-# tmp <- rbind(tmp, combinedResult)
-# combinedResult <- tmp
-# 
-# 
-# plotThemeReal <- theme_bw() +
-#   theme(plot.title = element_text(size=40, vjust = 2.5, face = "bold"),
-#         axis.title.x = element_text(margin = margin(15,0,0,0)),
-#         axis.title.y = element_text(margin = margin(0,15,0,0)),
-#         axis.text = element_text(margin = margin(0,0,0,0)),
-#         axis.ticks.y = element_blank(),
-#         axis.ticks.x = element_blank(),
-#         plot.margin = unit(c(0, 0, 0.3, 0), "lines"),
-#         panel.border = element_blank(),
-#         panel.grid.major.x = element_blank(),
-#         panel.grid.major.y = element_line( size=.3, color="black"),
-#         legend.key = element_rect(size=1, color="white"),
-#         legend.key.size = unit(1, "lines"),
-#         legend.key.height=unit(1,"lines"),
-#         legend.title = element_blank(),
-#         legend.position = "right",
-#         strip.background = element_blank(),
-#         strip.text = element_text(size = 12),
-#         panel.margin.x=unit(1.5, "lines"))
-# 
-# plotReal <- combinedResult %>%
-#   select(dataset, algorithm, AUC = AUC_sum) %>%
-#   mutate(AUC = 0.5 + abs(0.5 - AUC)) %>%
-#   group_by(dataset, algorithm) %>%
-#   slice(which.max(AUC)) # %>%
-#   # mutate(percentageDisplay = paste0("[", outlierPercentage, "%]")) %>%
-# 
-#    ggplot(aes(x=dataset , y=AUC, fill=algorithm)) +
-#   #ggplot(aes(x=percentageDisplay , y=AUC, fill=algorithm)) +
-#   geom_boxplot() +
-#   # facet_wrap(~data.name, nrow=1, scales = "free_x") +
-#   xlab(label = "Datasets") +
-#   ylab(label = "AUC") +
-#   plotThemeReal +
-#   scale_y_continuous(limits = c(0.5,1)) +
-#   scale_fill_grey(start = 0.1, end = 0.7) 
+
+
+## ---- auc
+  
+  combinedResult %>%   
+  select(dataset, algorithm, AUC_sum) %>%
+  
+  group_by(dataset, algorithm) %>%
+  slice(which.max(AUC_sum)) %>%
+  group_by(dataset, algorithm) %>%
+  summarize(mean.AUC = mean(AUC_sum), sd.AUC = sd(AUC_sum)) %>%
+  select(dataset, algorithm, mean.AUC, sd.AUC) %>%
+  
+  ggplot(aes(x = factor(dataset), y = mean.AUC, fill = algorithm)) +
+  geom_bar(position = position_dodge(), stat="identity", width = 0.7) +
+  geom_errorbar(aes(ymin=mean.AUC - sd.AUC, ymax=mean.AUC + sd.AUC), width=.2, position = position_dodge(.7)) +
+  xlab(label = "Dataset") +
+  ylab(label = "AUC") +
+  scale_fill_grey(start = 0.1, end = 0.7)
 
 ## ---- end
-   
-   
-filename <- "experiments_compos.tex"
-tikz(filename, standAlone = F, width=7, height=3)
-
-plotReal
-dev.off()
-
-plotReal
-
-
-# ## ---- (begin) Subspace Size
-plotSSSize <- combinedResult %>% 
-
-  # Boxplot of MPG by Car Cylinders
-  boxplot(combinedResult~subsp1,data=combinedResult, main="Subspace Size",
-          xlab="Subspace 1", ylab="Size") 
-
-plotBoxSSSize
-dev.off()
-plotBoxSSSize
-
-# ## ---- (end) Subspace Size
-
-
-
-
-
-
-
-
+  
+## ---- runtime
+  
+  combinedResult %>%   
+    select(dataset, algorithm, durationLOF, duationSS) %>%
+    
+    group_by(dataset, algorithm) %>%
+    slice(which.max(sum(duationSS, durationLOF))) %>%
+    group_by(dataset, algorithm) %>%
+    
+    mutate(runtime = sum(duationSS, durationLOF)) %>%
+    
+    ggplot(aes(x = factor(dataset), y = runtime, fill = algorithm)) +
+    geom_bar(position = position_dodge(), stat="identity", width = 0.7) +
+    xlab(label = "Dataset") +
+    ylab(label = "Runtime in sec.") +
+    scale_fill_brewer(palette = "Set2") 
+## ---- end
+  
+  
+  
+  
+  
+  
+## ---- meanSizeTop20SS 
+  ssv <- rbind(annSSU, diabSSU, glassSSU, ionSSU, lympSSU, penSSU, segmSSU)
+  
+  # gather first 
+  long_DF <- ssv %>% gather(name, subsp, subsp1V:subsp20V)
+  
+  
+  long_DF <- long_DF[order(long_DF$algorithm),]
+  
+  # subspace length
+  lengthSS <- c(length(long_DF$subsp[[1]]))
+  #   .. add length as new column "length"
+  for (i in 2:length(long_DF$subsp)){
+    lengthSS <- c(lengthSS, length(long_DF$subsp[[i]]))
+  }
+  subspaces <- mutate(long_DF, length = lengthSS) 
+  
+  # mean per algorithm per dataset
+  algdsDistinct <- subspaces %>% select(algorithm, dataset) %>% distinct
+  
+  dfMean <- data.frame()
+  for (i in 1:nrow(algdsDistinct)){
+    
+    tmpdf <- filter(subspaces, subspaces$algorithm == algdsDistinct$algorithm[i] & subspaces$dataset == algdsDistinct$dataset[i])             %>% select(length)
+    
+    tmpMean <- mean(tmpdf$length)
+    # print(tmpMean)
+    v <- data.frame(dataset = algdsDistinct$dataset[i], algorithm = algdsDistinct$algorithm[i], mean = tmpMean)
+    dfMean  <- rbind(dfMean, v)
+    
+  }
+  
+  
+  ggplot(dfMean, aes(factor(dataset), mean, fill=algorithm, x=dataset, y=mean, gg)) +
+    geom_bar(stat="identity", position = "dodge") + 
+    scale_fill_brewer(palette = "Set2") +
+    labs(title ="Average size of Top20-Subspaces", y = "Mean size") 
+## ---- end
+  
+  
+  
+## ---- meanSizeTop5SS
+  
+  top5Length <- filter(subspaces, subspaces$name == "subsp1V" | subspaces$name == "subsp2V" | subspaces$name == "subsp3V"
+                       | subspaces$name == "subsp4V" | subspaces$name == "subsp5V") 
+  
+  dfMean5 <- data.frame()
+  for (i in 1:nrow(algdsDistinct)){
+    
+    tmpdf5 <- filter(top5Length, top5Length$algorithm == algdsDistinct$algorithm[i] & top5Length$dataset == algdsDistinct$dataset[i])             %>% select(length)
+    
+    tmpMean5 <- mean(tmpdf5$length)
+    # print(tmpMean)
+    v <- data.frame(dataset = algdsDistinct$dataset[i], algorithm = algdsDistinct$algorithm[i], mean = tmpMean5)
+    dfMean5  <- rbind(dfMean5, v)
+  }
+  
+  ggplot(dfMean5, aes(factor(dataset), mean, fill=algorithm, x=dataset, y=mean, gg)) +
+    geom_bar(stat="identity", position = "dodge") + 
+    scale_fill_brewer(palette = "Set2") +
+    ggtitle("Average size of Top5-Subspaces")
+  
+## ---- end
+  
+## ---- sampleSS 
+  colNam <- c("Algorithm","Dataset","Subspace 1","Subspace 2","Subspace 3","Subspace 4","Subspace 5")
+  
+  kable(glass5SS, caption = "Top-5-Subspaces - Glass-Dataset", col.names = colNam)
+  kable(ion5SS, caption = "Top-5-Subspaces - Ion-Dataset", col.names = colNam)
+  kable(pen5SS, caption = "Top-5-Subspaces - Pendigits-Dataset", col.names = colNam)
+## ---- end
+  
+  
+  
+  
+## ---- meanSizealg
+    meanAlg <- data.frame()
+  SSCMI <- filter(subspaces, subspaces$algorithm == "CMI")
+  meanCMI <- mean(SSCMI$length)
+  nL <- data.frame(algorithm = "CMI", mean = meanCMI)
+  meanAlg <- rbind(meanAlg, nL)
+  
+  SSHics <- filter(subspaces, subspaces$algorithm == "HiCS")
+  meanHics <- mean(SSHics$length)
+  nL <- data.frame(algorithm = "HiCS", mean = meanHics)
+  meanAlg <- rbind(meanAlg, nL)
+  
+  SSGMD <- filter(subspaces, subspaces$algorithm == "GMD")
+  meanGMD <- mean(SSGMD$length)
+  nL <- data.frame(algorithm = "GMD", mean = meanGMD)
+  meanAlg <- rbind(meanAlg, nL)
+  
+  ggplot(meanAlg, mapping = aes(factor(algorithm), mean, x=algorithm, y=mean, gg)) +
+    geom_bar(stat="identity", position = "dodge") + 
+    scale_color_brewer(palette = "Set2") +
+    labs(title ="Average size of Top20-Subspaces per algorithm", y = "Mean size") 
+## ---- end
+  
